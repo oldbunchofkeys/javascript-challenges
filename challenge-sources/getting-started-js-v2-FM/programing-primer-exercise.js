a62d785b3f114d302fdeca925820e03d906045ee
Pete Thompson <peter.thompson@roswellstudios.com>
2022-04-08 12:43:54 -0400

    update challenge

--- challenge-sources/getting-started-js-v2-FM/programing-primer-exercise.js
+++ challenge-sources/getting-started-js-v2-FM/programing-primer-exercise.js
@@ -2,9 +2,12 @@
 // 2) If the provided `bookName` string does *NOT* have the word "Great" in it, add it to the favoriteBooks array.
 // hint:
 // stringOne.includes(stringTwo) will return `true` if `anotherString` is found inside `someString`, or `false` if not.
+// 3) Definte a `printFavoriteBooks()` function that receives no parameters.
+// 4) `printFavoriteBooks()` should first print a message like "Favorite books" and include the number of books in the favoriteBooks array.
+// 5) Finally, printFavoriteBooks() should loop through the favoriteBooks array and print out each value.
 const favoriteBooks = [];
 const addFavoriteBook = function(bookName) {
-  if (bookName.includes('Great')=== false) {
+  if (bookName.includes('Great') === false) {
     favoriteBooks.push(bookName);
   }
 }
