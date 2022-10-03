2e2e033b36af7d7b4d35d19e5e46dcdfd867b386
Pete Thompson <peter.thompson@roswellstudios.com>
2022-10-03 17:52:20 -0400

    feat: add challenge

---
+++ challenge-sources/codewars/reverseEveryOther.js
@@ -0,0 +1,22 @@
+//Reverse every other word in a given string, 
+//then return the string. Throw away any leading 
+//or trailing whitespace, while ensuring there 
+//is exactly one space between each word. 
+//Punctuation marks should be treated as if 
+//they are a part of the word in this kata.
+
+function reverse(str){
+  
+  let words = str.trim().split(' ');
+  let returnWords = [];
+  for (let i = 0; i < words.length; i++) {
+    if (i % 2 !== 0) { returnWords.push(words[i].split('').reverse().join().replace(/,/g, ''));
+    } else {
+      returnWords.push(words[i]);
+    }
+  }
+  if (str === ',') {
+    return '';
+  }
+  return returnWords.join().replace(/,/g, ' ').replace(/  /, ', ')
+}
